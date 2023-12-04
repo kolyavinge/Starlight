7bec522b3613a0087abface07c0a9539c9e75692
Nikolay Vinge <vinge@megatec.ru>
2023-12-04 12:32:23 +0400

    include open gl headers in project

--- src/lib/Exceptions.h
+++ src/lib/Exceptions.h
@@ -7,3 +7,5 @@ class Exception : public Object {};
 class IndexOutOfBoundsException : public Exception {};
 
 class MemoryAllocationException : public Exception {};
+
+class AssertException : public Exception {};
