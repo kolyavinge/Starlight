315a09df653ae64bacac19fbc693479ba125030b
Nikolay Vinge <vinge@megatec.ru>
2024-01-23 14:18:53 +0400

    VectorCalculator GetMinVector(), GetMaxVector()

--- src/calc/VectorCalculator.cpp
+++ src/calc/VectorCalculator.cpp
@@ -1,3 +1,4 @@
+#include <lib/Exceptions.h>
 #include <lib/Math.h>
 #include <lib/Numeric.h>
 #include <calc/VectorCalculator.h>
@@ -59,4 +60,42 @@ bool VectorCalculator::IsSameDirection(Vector3& base, Vector3 v1, Vector3 v2)
     v1.Sub(v2);
 
     return v1.GetLengthSquared() < 0.1f;
+}
+
+Vector3 VectorCalculator::GetMinVector(List<Vector3>& vectors)
+{
+    if (vectors.Count() == 0) throw ArgumentException();
+
+    float minX = vectors[0].X;
+    float minY = vectors[0].Y;
+    float minZ = vectors[0].Z;
+
+    for (int i = 1; i < vectors.Count(); i++)
+    {
+        Vector3& v = vectors[i];
+        if (v.X < minX) minX = v.X;
+        if (v.Y < minY) minY = v.Y;
+        if (v.Z < minZ) minZ = v.Z;
+    }
+
+    return Vector3(minX, minY, minZ);
+}
+
+Vector3 VectorCalculator::GetMaxVector(List<Vector3>& vectors)
+{
+    if (vectors.Count() == 0) throw ArgumentException();
+
+    float maxX = vectors[0].X;
+    float maxY = vectors[0].Y;
+    float maxZ = vectors[0].Z;
+
+    for (int i = 1; i < vectors.Count(); i++)
+    {
+        Vector3& v = vectors[i];
+        if (v.X > maxX) maxX = v.X;
+        if (v.Y > maxY) maxY = v.Y;
+        if (v.Z > maxZ) maxZ = v.Z;
+    }
+
+    return Vector3(maxX, maxY, maxZ);
 }
--- src/calc/VectorCalculator.h
+++ src/calc/VectorCalculator.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <lib/List.h>
 #include <calc/Vector3.h>
 
 class VectorCalculator
@@ -11,4 +12,6 @@ public:
         float* resultX, float* resultY);
     static void GetNormalVector3d(Vector3& center, Vector3 right, Vector3 front, Vector3& result);
     static bool IsSameDirection(Vector3& base, Vector3 v1, Vector3 v2);
+    static Vector3 GetMinVector(List<Vector3>& vectors);
+    static Vector3 GetMaxVector(List<Vector3>& vectors);
 };
