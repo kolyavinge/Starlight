150e12e89b9bacfda2dfd2a8ed373565c725b6c4
Nikolay Vinge <vinge@megatec.ru>
2024-01-29 10:05:38 +0400

    CycledArrayIterator

---
+++ src/lib/CycledArrayIterator.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <lib/Object.h>
+#include <lib/Array.h>
+
+template<class T, int Count>
+class CycledArrayIterator : public Object
+{
+    Array<T, Count>& _array;
+    int _index;
+
+public:
+    CycledArrayIterator(Array<T, Count>& array) :
+        _array(array)
+    {
+        _index = 0;
+    }
+
+    T& GetCurrentItem()
+    {
+        return _array[_index];
+    }
+
+    void Prev()
+    {
+        _index--;
+        if (_index == -1) _index = Count - 1;
+    }
+
+    void Next()
+    {
+        _index++;
+        if (_index == Count) _index = 0;
+    }
+};
