d0de5597d0ac7ae5fc11e363f09a0b84329b519b
Nikolay Vinge <vinge@megatec.ru>
2024-02-01 14:59:06 +0400

    File new methods

--- src/lib/File.cpp
+++ src/lib/File.cpp
@@ -21,3 +21,26 @@ void File::ErrorIfFileNotFound(const wchar_t* filePath)
         throw new FileNotFoundException();
     }
 }
+
+int File::GetFileSizeBytes(const wchar_t* filePath)
+{
+    ErrorIfFileNotFound(filePath);
+    WIN32_FILE_ATTRIBUTE_DATA fad;
+    GetFileAttributesEx(filePath, GetFileExInfoStandard, &fad);
+    LARGE_INTEGER size;
+    size.HighPart = fad.nFileSizeHigh;
+    size.LowPart = fad.nFileSizeLow;
+
+    return size.QuadPart;
+}
+
+int File::ReadAllBytes(const wchar_t* filePath, int bufferSize, void* buffer)
+{
+    ErrorIfFileNotFound(filePath);
+    DWORD readedBytes = 0;
+    HANDLE hFile = CreateFile(filePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    bool readResult = ReadFile(hFile, buffer, bufferSize, &readedBytes, NULL);
+    if (!readResult) throw ReadFileException();
+
+    return readedBytes;
+}
--- src/lib/File.h
+++ src/lib/File.h
@@ -3,10 +3,13 @@
 #include <lib/Exceptions.h>
 
 class FileNotFoundException : public Exception { };
+class ReadFileException : public Exception { };
 
 class File
 {
 public:
     static bool Exists(const wchar_t* filePath);
     static void ErrorIfFileNotFound(const wchar_t* filePath);
+    static int GetFileSizeBytes(const wchar_t* filePath);
+    static int ReadAllBytes(const wchar_t* filePath, int bufferSize, void* buffer);
 };
