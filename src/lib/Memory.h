632b3e03aca4e69e109c13b38a117947569f0191
Vinge Nikolay <vn2756@gmail.com>
2024-03-03 19:14:15 +0400

    Memory fix

--- src/lib/Memory.h
+++ src/lib/Memory.h
@@ -10,8 +10,9 @@ class Memory
 {
 public:
     template<class T>
-    static T* AllocAndZero(unsigned int itemsCount)
+    static T* AllocAndZero(int itemsCount)
     {
+        if (itemsCount <= 0) throw ArgumentException();
         T* result = (T*)malloc(itemsCount * sizeof(T));
         if (result == nullptr) throw MemoryAllocationException();
         Zero<T>(result, itemsCount);
@@ -20,14 +21,16 @@ public:
     }
 
     template<class T>
-    static void Zero(T* source, unsigned int itemsCount)
+    static void Zero(T* source, int itemsCount)
     {
+        if (itemsCount <= 0) throw ArgumentException();
         std::memset(source, 0, itemsCount * sizeof(T));
     }
 
     template<class T>
-    static void Copy(const T* source, T* dest, unsigned int itemsCount)
+    static void Copy(const T* source, T* dest, int itemsCount)
     {
+        if (itemsCount < 0) throw ArgumentException();
         std::memcpy(dest, source, itemsCount * sizeof(T));
     }
 
@@ -37,7 +40,7 @@ public:
     }
 
     template<class T>
-    static void Resize(T*& source, unsigned int currentItemsCount, unsigned int newItemsCount)
+    static void Resize(T*& source, int currentItemsCount, int newItemsCount)
     {
         T* newSource = AllocAndZero<T>(newItemsCount);
         Copy<T>(source, newSource, currentItemsCount);
