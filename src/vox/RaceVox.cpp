c21e6db907ad2b70dadad1eb44e3f6275e21c074
Nikolay Vinge <vinge@megatec.ru>
2024-04-08 12:50:08 +0400

    3d sound effect

--- src/vox/RaceVox.cpp
+++ src/vox/RaceVox.cpp
@@ -1,49 +1,70 @@
+#include <lib/Object.h>
+#include <calc/Vector3.h>
 #include <model/Ship.h>
-#include <core/Race.h>
 #include <ui/RaceScreen.h>
 #include <vox/RaceVox.h>
 
-RaceVox::RaceVox(AudioDataCollection& audioDataCollection) :
-    _weaponFireSample(audioDataCollection.LaserData),
-    _metalHitSample(audioDataCollection.MetalHitData),
-    _explosionSample(audioDataCollection.ExplosionData),
-    _powerUpPickSample(audioDataCollection.PowerUpPickData)
+RaceVox::RaceVox(AudioDataCollection& audioDataCollection)
 {
+    for (int i = 0; i < SamplesCount; i++)
+    {
+        _weaponFireSamples[i].SetData(audioDataCollection.LaserData);
+        _metalHitSamples[i].SetData(audioDataCollection.MetalHitData);
+        _explosionSamples[i].SetData(audioDataCollection.ExplosionData);
+        _powerUpPickSamples[i].SetData(audioDataCollection.PowerUpPickData);
+    }
 }
 
 void RaceVox::Voice(Screen& screen)
 {
     RaceScreen& raceScreen = (RaceScreen&)screen;
     Race& race = raceScreen.Race;
+    Ship& player = race.Player;
 
     for (int i = 0; i < race.AllShips.GetCount(); i++)
     {
         Ship& ship = *race.AllShips[i];
 
+        if (!Object::ReferenceEquals(player, ship))
+        {
+            SetSamplePosition(player, ship, _metalHitSamples[i]);
+            SetSamplePosition(player, ship, _explosionSamples[i]);
+            SetSamplePosition(player, ship, _weaponFireSamples[i]);
+            SetSamplePosition(player, ship, _powerUpPickSamples[i]);
+        }
+
         if (ship.IsDamaged)
         {
-            _metalHitSample.SetGain(0.2f);
-            _metalHitSample.Play();
+            _metalHitSamples[i].SetGain(0.4f);
+            _metalHitSamples[i].Play();
         }
 
         if (ship.State == ShipState::Exploded)
         {
-            _explosionSample.Play();
+            _explosionSamples[i].Play();
         }
 
         if (ship.Weapon.IsFireActive)
         {
-            _weaponFireSample.SetGain(0.2f);
-            _weaponFireSample.Play();
+            _weaponFireSamples[i].SetGain(0.2f);
+            _weaponFireSamples[i].Play();
         }
 
         if (ship.IsPowerUpPicked)
         {
-            _powerUpPickSample.SetGain(1.5f);
-            _powerUpPickSample.SetPitch(2.0f);
-            _powerUpPickSample.Play();
+            _powerUpPickSamples[i].SetGain(1.5f);
+            _powerUpPickSamples[i].SetPitch(2.0f);
+            _powerUpPickSamples[i].Play();
         }
     }
+}
+
+void RaceVox::SetSamplePosition(Ship& player, Ship& anotherShip, AudioSample& sample)
+{
+    Vector3 position(anotherShip.CentralLine.Front);
+    position.Sub(player.CentralLine.Front);
+    position.Mul(0.1f);
+    sample.SetPosition(position);
 }
 
 RaceVox* RaceVoxResolvingFactory::Make(Resolver& resolver)
--- src/vox/RaceVox.h
+++ src/vox/RaceVox.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <lib/Array.h>
 #include <lib/di/ResolvingFactory.h>
+#include <model/Ship.h>
+#include <core/Race.h>
 #include <audio/AudioSample.h>
 #include <ui/Screen.h>
 #include <vox/ScreenVox.h>
@@ -8,15 +11,20 @@
 
 class RaceVox : public ScreenVox
 {
-    AudioSample _weaponFireSample;
-    AudioSample _metalHitSample;
-    AudioSample _explosionSample;
-    AudioSample _powerUpPickSample;
+    inline static const int SamplesCount = Race::EnemiesCount + 1;
+
+    Array<AudioSample, SamplesCount> _weaponFireSamples;
+    Array<AudioSample, SamplesCount> _metalHitSamples;
+    Array<AudioSample, SamplesCount> _explosionSamples;
+    Array<AudioSample, SamplesCount> _powerUpPickSamples;
 
 public:
     RaceVox(AudioDataCollection& audioDataCollection);
 
     void Voice(Screen& screen) override;
+
+private:
+    void SetSamplePosition(Ship& player, Ship& anotherShip, AudioSample& sample);
 };
 
 class RaceVoxResolvingFactory : public ResolvingFactory<RaceVox>
