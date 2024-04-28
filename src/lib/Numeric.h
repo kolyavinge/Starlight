5c9472b9801d8515bcf7511da6829408d71ce9c8
Vinge Nikolay <vn2756@gmail.com>
2024-04-28 20:23:01 +0400

    ShipCollisionDetector CheckBordersZ

--- src/lib/Numeric.h
+++ src/lib/Numeric.h
@@ -5,4 +5,5 @@ class Numeric
 public:
     static bool FloatEquals(float a, float b, float eps = 1e-7f);
     static bool Between(int x, int left, int right);
+    static bool Between(float x, float left, float right);
 };
